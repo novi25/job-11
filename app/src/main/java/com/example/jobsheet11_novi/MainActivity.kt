45b4d59da1bf4b986a17d458669e0b7e3c57a31a
fifitt <tiarabintang@gmail.com>
2022-09-13 08:28:42 +0700

    first commit

---
+++ app/src/main/java/com/example/jobsheet11_novi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jobsheet11_novi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
